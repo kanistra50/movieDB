e4e31ff6278f7db5cf5b92e39087e4cd179df9ca
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-02-07 08:18:10 +0200

    Update myList.js

--- js/myList.js
+++ js/myList.js
@@ -1,6 +1,3 @@
-/**
- * Created by nickon on 30.01.2017.
- */
 //https://developers.themoviedb.org/3/search/search-movies
 (function (m) {
     'use strict';
@@ -8,9 +5,6 @@
     m.Favorites = ng.core.Component({
 
         templateUrl: 'html/myList.html'
-
-
-
 
     })
         .Class({
@@ -21,36 +15,57 @@
             ]
         });
 
+    m.movies = [];
+
     function Favorites( http, route ) {
+        var t = this,
+            id = route.params.getValue().id;
 
         Object.assign(this, {
             http: http,
-            foundMovies: [],
-            id_base: [],
-            genres: []
+            movies: m.movies,
+            remove: function (id) {m.Ls.removeFav(id); console.log("Remove on the myListJS")}
+ 
         });
 
     }
 
-    // MovieList.prototype.loadList = function () {
-    //     var t = this;
-    //     this.http.getMovies(1)
-    //         .then(function (list) {
-    //             t.movies = list.results;
-    //             //console.log(t.movies);
-    //         })
-    //         .catch(function (err) { console.log(err)})
-    //
-    //     this.http.getGenres()
-    //         .then(function (list) {
-    //             t.genres = list.genres;
-    //
-    //         })
-    //         .catch(function (err) { console.log(err)})
-    // };
+     Favorites.prototype.loadList = function () {
+         var list = JSON.parse(localStorage.getItem('movieDB')) || {
+                 favs: []
+             },
+             ls = list["favs"],
+             movies = m.movies;
+
+
+            if (ls[0] != undefined) {
+                for (var i in ls) {
+                    console.log(ls[i]);
+
+                    this.http.getMovie(ls[i])
+                        .then(function (movie) {
+                            m.movies.push(movie);
+                        })
+                        .catch(function (err) {console.log(err)})
+                };
+            };
+
+            console.log(m.movies);
+            return m.movies;
+  
+    };
 
     Favorites.prototype.navigate = function (path) {
         m.AppComponent.router.navigateByUrl(path);
     };
+
+    Favorites.prototype.ngOnInit = function () {
+        this.loadList();
+    };
+
+
+    Favorites.prototype.ngAfterViewInit = function () {};
+
+    Favorites.prototype.ngOnDestroy = function () {};
 
 })(window.m || (window.m = {}));
