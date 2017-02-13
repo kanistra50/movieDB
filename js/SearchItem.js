86cd70e8cc346e7118454d3069463535e6cbd622
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-02-13 12:54:49 +0200

    Update SearchItem.js

--- js/SearchItem.js
+++ js/SearchItem.js
@@ -9,51 +9,64 @@
     })
         .Class({
             constructor: [
+                m.LsController,
                 m.HttpService,
                 ng.router.ActivatedRoute,
                 Search
             ]
         });
 
-    function Search( http, route) {
-        var search = route;
+
+    function Search(ls, http, route) {
+
         Object.assign(this, {
+            ls: ls,
             http: http,
-            done: m.done,
-            add: m.add(),
+            key_word: '',
+            include_adult: false,
             foundMovies: [],
-            key_word: 'One',
-            // genres: []
-        });
+            ar: [],
+            page_id: 0,
+            active: ""
+        })
 
     }
 
     Search.prototype.startSearch = function () {
         var t = this;
-        console.log(this);
-        this.http.getSearch()
+        this.http.getSearch(t.key_word, t.include_adult)
             .then(function (list) {
-                t.foundMovies = list.results;
-                console.log(t.foundMovies);
+
+                var len = list.results.length,
+                    n;
+
+                if(len >3) {
+                    var nId=0,
+                        left = len%3;
+                    n = parseInt(len/3);
+
+                    for (var ind = 0; ind<n; ind++) {
+                        t.ar[ind]=list.results.slice(3*ind, 3*ind + 3);
+                    }
+
+                    if (left != 0 ) {
+                        t.ar[t.ar.length] = list.results.slice(len - left, len );
+                    }
+
+                    // if( page_id == nId ) {
+                    //     t.active = "active";
+                    // }
+
+                    t.foundMovies = t.ar[nId];
+                } else {
+                    t.ar= [];
+                    t.foundMovies = list.results;
+
+                }
             })
-            .catch(function (err) { console.log(err)})
-
-        // this.http.getGenres()
-        //     .then(function (list) {
-        //         t.genres = list.genres;
-        //         console.log(t.genres);
-        //     })
-        //     .catch(function (err) { console.log(err)})
+            .catch(function (err) { console.log(err)});
     };
 
-    /* function validation (key_word, include_adult) {
-
-        if (key_word) {
-            console.log("Validation - " + key_word);
-            Search.prototype.startSearch(key_word, include_adult);
-        }
-    };
-    */
 
 
     Search.prototype.navigate = function (path) {
