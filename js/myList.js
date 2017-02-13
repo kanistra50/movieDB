f375a58b6291a7afe5815cb74f86b17ba063fcb7
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-02-13 12:55:24 +0200

    Update myList.js

--- js/myList.js
+++ js/myList.js
@@ -1,77 +1,50 @@
-/**
- * Created by nickon on 30.01.2017.
- */
 //https://developers.themoviedb.org/3/search/search-movies
 (function (m) {
     'use strict';
 
     m.Favorites = ng.core.Component({
-
         templateUrl: 'html/myList.html'
-
     })
         .Class({
             constructor: [
                 m.HttpService,
-                ng.router.ActivatedRoute,
+                m.LsController,
                 Favorites
             ]
         });
 
-    m.movies = [];
-
-    function Favorites( http, route ) {
-        var t = this,
-            id = route.params.getValue().id;
-
+    function Favorites(http, ls) {
         Object.assign(this, {
+            ls: ls,
             http: http,
-            movies: m.movies,
-            //remove: function (id) {m.remove(id)}
-            remove: function (id) {m.Ls.removeFav(id); console.log("Remove on the myListJS")}
-            //done: m.done,
-            //add: function (id) {m.add(id)},
-            // id: id,
-            // item: { id : 'id', title : 'title', popularity: 'popularity',  overview : 'overview',
-            //     backdrop_path : 'backdrop_path'}
+            movies: []
+        });
+    }
+    Favorites.prototype.remove = function (id) {
+        var n = this.movies.find(function (el) {
+            return el.id === id;
         });
 
-        // this.subOnIdChange = route.params.subscribe(function(p) {
-        //     t.id = parseInt(p.id);
-        //     t.loadItem(p.id);
-        // });
-
-    }
-
-
+        if (n) {
+            var nId = this.movies.indexOf(n);
+            this.movies.splice(nId, 1);
+        }
+        this.ls.removeFav(id);
+    };
 
      Favorites.prototype.loadList = function () {
+         var t = this,
+             list = t.ls.getFav();
 
-         var //t = this,
-             list = JSON.parse(localStorage.getItem('movieDB')) || {
-                 favs: []
-             },
-             ls = list["favs"],
-             movies = m.movies;
-
-            //console.log(ls);
-
-            if (ls[0] != undefined) {
-                m.movies = [];
-                for (var i in ls) {
-                    console.log(ls[i]);
-
-                    this.http.getMovie(ls[i])
-                        .then(function (movie) {
-                            m.movies.push(movie);
-                        })
-                        .catch(function (err) {console.log(err)})
-                };
-            };
-
-            console.log(m.movies);
-            return m.movies;
-
+         list.forEach(function(el){
+                t.http.getMovie(el)
+                    .then(function (movie) {
+                        t.movies.push(movie);
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                    })
+            });
     };
 
     Favorites.prototype.navigate = function (path) {
@@ -81,10 +54,6 @@
     Favorites.prototype.ngOnInit = function () {
         this.loadList();
     };
-
-
-    // Favorites.prototype.ngAfterContentInit = function () {
-    // };
 
     Favorites.prototype.ngOnDestroy = function () {};
 
